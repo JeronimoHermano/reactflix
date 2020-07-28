57d077c20abc7bd16e5cf1ff08030b82a7627915
Jeronimo Hermano <jeronimohermano.nc@gmail.com>
2020-07-28 15:13:25 -0300

    Alteração do carousel para utilizar a lib react-slick.

--- reactflix/src/components/Carousel/index.tsx
+++ reactflix/src/components/Carousel/index.tsx
@@ -3,13 +3,14 @@ import { VideoCardGroupContainer, VideoCardList, Title, ExtraLink } from './styl
 import VideoCard from './components/VideoCard';
 
 import Category from '../../data/DadosIniciais'
+import Slider, { SliderItem } from '../Slider';
 
 interface Props {
   ignoreFirstVideo: boolean
   category: Category
 }
 
-const VideoCardGroup = ({ignoreFirstVideo,category}:Props) => {
+const Carousel = ({ignoreFirstVideo,category}:Props) => {
   const categoryTitle = category.titulo;
   const categoryColor = category.cor;
   const categoryExtraLink = category.link_extra;
@@ -28,25 +29,25 @@ const VideoCardGroup = ({ignoreFirstVideo,category}:Props) => {
           }
         </>
       )}
-      <VideoCardList>
+      <Slider>
         {videos.map((video, index) => {
           if (ignoreFirstVideo && index === 0) {
             return null;
           }
 
           return (
-            <li key={video.titulo}>
+            <SliderItem key={video.titulo}>
               <VideoCard
                 videoTitle={video.titulo}
                 videoURL={video.url}
                 categoryColor={categoryColor}
               />
-            </li>
+            </SliderItem>
           );
         })}
-      </VideoCardList>
+      </Slider>
     </VideoCardGroupContainer>
   );
 }
 
-export default VideoCardGroup;
+export default Carousel;
